3d19fd5eb3ea22bc90ff2f596e3f7a5d98c0e278
TimatoPaste <tmthlou@gmail.com>
2023-06-06 14:05:04 -0700

    Added connection b/w front and backend for resources component

--- chem-site/src/app/resources/resources.component.ts
+++ chem-site/src/app/resources/resources.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import * as Parse from 'parse';
+
+Parse.initialize('FgJsXpqyLH852BBw5DnuccJOUXLMsysjccWNcBIC', 'h6TiU187AcIgJfQN9sRXildBMvUFiIiBmUP3GtTm');
+(Parse as any).serverURL = 'https://parseapi.back4app.com/';
 
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
   styleUrls: ['./resources.component.scss']
 })
-export class ResourcesComponent {
 
+export class ResourcesComponent implements OnInit{
+	ngOnInit(){
+		let htmlList = document.getElementById("sourcesList");
+		let parseClass = Parse.Object.extend("Links");
+		let parseQuery = new Parse.Query(parseClass);
+		parseQuery.equalTo("Approved","True");
+		
+		parseQuery.find()
+			.then((results)=>{
+				
+				for(let a = 0;a<results.length;a++){
+					let newLi = document.createElement("li");
+					let newA = document.createElement("a");
+					
+					newA.href = results[a].get("Link");
+					newA.title = results[a].get("resourceName");
+					
+					console.log(results[a].get("Link"));
+					
+					newA.appendChild(document.createTextNode(results[a].get("resourceName")));
+					newLi.appendChild(newA);
+					htmlList!.appendChild(newLi);
+				}
+				
+			})
+			.catch((error)=>{
+				console.log(error);
+			})
+	}
 }
+
+
+
